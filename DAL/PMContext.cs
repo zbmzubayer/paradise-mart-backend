546b3aa67a65fa632a48a1213ccdf49b36aa4005
ZBM Zubayer <zbmzubayer@gmail.com>
2023-05-01 02:27:13 +0600

    DbContext added

---
+++ DAL/PMContext.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PMContext : DbContext
+    {
+        public DbSet<Customer> Customers {  get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductPhoto> ProductPhotos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<CustomerPayment> CustomerPayments { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
