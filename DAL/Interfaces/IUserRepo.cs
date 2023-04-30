338d038794d0a4dde56beaa827faab00737f25cc
ZBM Zubayer <zbmzubayer@gmail.com>
2023-05-01 02:29:57 +0600

    Interfaces for repositories

---
+++ DAL/Interfaces/IUserRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUserRepo<TYPE>
+    {
+        TYPE Get(int id);
+        TYPE GetByEmail(string email);
+        bool ChangePassword(string guid, string password);
+    }
+}
