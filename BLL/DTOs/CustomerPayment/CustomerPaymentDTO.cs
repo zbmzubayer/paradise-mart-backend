72e756aca7133732f3aa30ccc6b846c900f3e40d
ZBM Zubayer <zbmzubayer@gmail.com>
2023-05-13 13:55:16 +0600

    CustomerPayment DTO

---
+++ BLL/DTOs/CustomerPayment/CustomerPaymentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs.CustomerPayment
+{
+    public class CustomerPaymentDTO
+    {
+        public int Id { get; set; }
+        public string AccountNo { get; set; }
+        public DateTime ExpiredAt { get; set; }
+        public int PaymentId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
